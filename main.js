338c653af03f433861949e568e1fe695b91a939c
Daniel Montiel <danielponce1397@gmail.com>
2024-03-03 10:53:46 -0600

    Se agrego CV

--- main.js
+++ main.js
@@ -22,3 +22,32 @@ function seleccionado(link){
     const nav = document.querySelector(".navegador");
     nav.classList.remove("responsive");
 }
+
+window.onscroll = ()=>{
+    efectoHabilidades();
+};
+
+function efectoHabilidades(){
+    const habilidad = document.querySelector("#habilidades");
+    console.log(habilidad.getBoundingClientRect().top);
+    const distancia = window.innerHeight - habilidad.getBoundingClientRect().top;
+
+    if(distancia>=300){
+        document.querySelector("#html").classList.add("progreso1");
+        document.querySelector("#bootstrap").classList.add("progreso2");
+        document.querySelector("#js").classList.add("progreso3");
+        document.querySelector("#php").classList.add("progreso4");
+        document.querySelector("#git").classList.add("progreso5");
+        document.querySelector("#java").classList.add("progreso6");
+        document.querySelector("#c").classList.add("progreso7");
+    } else{
+        document.querySelector("#html").classList.remove("progreso1");
+        document.querySelector("#bootstrap").classList.remove("progreso2");
+        document.querySelector("#js").classList.remove("progreso3");
+        document.querySelector("#php").classList.remove("progreso4");
+        document.querySelector("#git").classList.remove("progreso5");
+        document.querySelector("#java").classList.remove("progreso6");
+        document.querySelector("#c").classList.remove("progreso7");
+    }
+
+}
